4dc4b5f839b18b43bf0b0a60fcb0c83dbdeeea49
cloaked04 <iownpratyush@gmail.com>
2025-08-09 12:02:53 -0400

    Adding multitable env, some options and improvements to diff drive.

--- predicators/envs/multitasking_room_env.py
+++ predicators/envs/multitasking_room_env.py
@@ -24,9 +24,9 @@ class FixedTasksMyDomainEnv(_FixedTasksPDDLEnv):
     def _train_problem_indices(self) -> List[int]:
         # List the indices for training tasks.
         # For example, if you have one fixed problem file:
-        return [2]
+        return [0]
 
     @property
     def _test_problem_indices(self) -> List[int]:
         # Similarly, list indices for testing.
-        return [2]
+        return [0]
